f3e3df434071e32042567ca500af8dd6185b26ef
Maxwell  Kwarteng <maxwellkwarteng7@gmail.com>
2024-09-25 17:16:38 +0000

    --finished the ui of the login page --

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { LoginInfo } from '../models/class';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterLink , FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  inputType = signal('password'); 
+
+  personDetails: LoginInfo = new LoginInfo(); 
+
+  changeInputType() {
+    if (this.inputType() == 'password') {
+      this.inputType.set('text'); 
+    } else {
+      this.inputType.set('password'); 
+    }
+  }
+
+  handleLoginSubmission() {
+    console.log(this.personDetails);
+    this.personDetails = new LoginInfo(); 
+  }
 
 }
---
+++ src/app/models/class.ts
@@ -0,0 +1,8 @@
+export class LoginInfo {
+    username: string; 
+    password: string; 
+    constructor() {
+        this.username = '';
+        this.password = ''; 
+    }
+}
