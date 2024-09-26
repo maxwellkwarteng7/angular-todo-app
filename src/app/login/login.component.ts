203db875643aac259f560d440b4037568f2e6f5f
Maxwell  Kwarteng <maxwellkwarteng7@gmail.com>
2024-09-26 16:18:54 +0000

    --added validation to registration form --

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LoginInfo } from '../models/class';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule  ,ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink , FormsModule , ],
+  imports: [RouterLink , FormsModule , ReactiveFormsModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -14,6 +14,10 @@ export class LoginComponent {
   inputType = signal('password'); 
 
   personDetails: LoginInfo = new LoginInfo(); 
+  loginForm: FormGroup = new FormGroup({
+    username: new FormControl('', [Validators.required]), 
+    password: new FormControl('', [Validators.required])
+  });
 
   changeInputType() {
     if (this.inputType() == 'password') {
@@ -23,9 +27,14 @@ export class LoginComponent {
     }
   }
 
+ 
+
   handleLoginSubmission() {
-    console.log(this.personDetails);
-    this.personDetails = new LoginInfo(); 
+    let loginDetails = this.loginForm.value; 
+    this.loginForm.reset({
+      username: loginDetails.username,
+      password : ''
+    }); 
   }
 
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,30 +1,89 @@
-import { Component , signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { CommonModule } from "@angular/common";
+import { Component, signal  , OnInit} from "@angular/core";
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidatorFn,
+  Validators,
+  ValidationErrors
+} from "@angular/forms";
+import { RouterLink } from "@angular/router";
+
+
+
 
 @Component({
-  selector: 'app-register',
+  selector: "app-register",
   standalone: true,
-  imports: [RouterLink],
-  templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  imports: [RouterLink, ReactiveFormsModule, CommonModule],
+  templateUrl: "./register.component.html",
+  styleUrl: "./register.component.scss",
 })
 export class RegisterComponent {
-  inputType = signal('password'); 
-  inputType1 = signal('password'); 
+  inputType = signal("password");
+  inputType1 = signal("password");
+  // creating the form group to handle the registration form
+  registerForm: FormGroup = new FormGroup({
+    username: new FormControl("", [Validators.required, Validators.maxLength(30)]),
+    password: new FormControl("", [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    confirmPassword: new FormControl("", [Validators.required]),
+  }, {
+    validators : this.passwordMatchValidator()
+  } );
 
-  changeInputType(val : string ) {
-    if (val == 'password') {
-      if (this.inputType() == 'password') {
-        this.inputType.set('text'); 
+
+  passwordMatchValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      // Check if the control is a FormGroup
+      if (control instanceof FormGroup) {
+        const password = control.get('password')?.value;
+        const confirmPassword = control.get('confirmPassword')?.value;
+  
+        // Return an error object if passwords do not match
+        return password && confirmPassword && password !== confirmPassword
+          ? { mismatch: true }
+          : null; // Return null if passwords match
+      }
+      return null; // Return null if control is not a FormGroup
+    };
+  }
+  changeInputType(val: string) {
+    if (val == "password") {
+      if (this.inputType() == "password") {
+        this.inputType.set("text");
       } else {
-        this.inputType.set('password'); 
+        this.inputType.set("password");
       }
     } else {
-      if (this.inputType1() == 'password') {
-        this.inputType1.set('text'); 
+      if (this.inputType1() == "password") {
+        this.inputType1.set("text");
       } else {
-        this.inputType1.set('password'); 
+        this.inputType1.set("password");
       }
     }
   }
+
+
+
+  // submit registration form data
+  handleRegistration() {
+    console.log(this.registerForm.controls); 
+    let registrationInputs = this.registerForm.value;
+    console.log(registrationInputs);
+  }
+
+  // a function to check if the passwords match
+ 
+
+  // return the form controls
+ 
+  get field(){
+    return this.registerForm.controls; 
+  }
+ 
 }
