3ee136e3ff6d1e6c364ab9eb216b00d28c454587
Maxwell  Kwarteng <maxwellkwarteng7@gmail.com>
2024-09-28 12:29:33 +0000

    --added a notification for confirmation and success messages --

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,120 +1,130 @@
-import { CommonModule } from '@angular/common';
-import { Component  , OnInit, signal, Signal} from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { getAllUserTodos, getUsernameAsKey } from '../models/data';
-import { Router } from '@angular/router';
-import { todo } from '../models/interface';
+import { CommonModule } from "@angular/common";
+import { Component, OnInit, signal, Signal } from "@angular/core";
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
+import { getAllUserTodos, getUsernameAsKey } from "../models/data";
+import { Router } from "@angular/router";
+import { todo } from "../models/interface";
+import { NotificationService } from "../services/notification.service";
 
 @Component({
-  selector: 'app-todo',
+  selector: "app-todo",
   standalone: true,
-  imports: [ReactiveFormsModule , CommonModule],
-  templateUrl: './todo.component.html',
-  styleUrl: './todo.component.scss'
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: "./todo.component.html",
+  styleUrl: "./todo.component.scss",
 })
 export class TodoComponent implements OnInit {
-  
-  userTodos = signal<todo [] | null >([]); 
+  constructor(private router: Router, private notify: NotificationService) {}
 
-  constructor(private router: Router) { }
+  userTodos = signal<todo[] | null>([]);
 
-  
   ngOnInit(): void {
-    this.userTodos.set(getAllUserTodos());  
-    console.log(this.userTodos()); 
+    this.userTodos.set(getAllUserTodos());
+    console.log(this.userTodos());
   }
 
-  
-  
-  clicked: boolean = false; 
+  clicked: boolean = false;
 
   todoForm: FormGroup = new FormGroup({
-    todo: new FormControl('', [Validators.required])
+    todo: new FormControl("", [Validators.required]),
   });
 
-
-  completed() {
-    this.clicked = !this.clicked; 
+  completed(val: number) {
+    this.clicked = !this.clicked;
   }
 
   get f() {
-    return this.todoForm.controls; 
+    return this.todoForm.controls;
   }
 
-  // key generator 
+  // key generator
   generateKey() {
-    let id = Math.ceil(Math.random() * 1000); 
-    return id; 
+    let id = Math.ceil(Math.random() * 1000);
+    return id;
   }
 
-  //reset the form 
+  //reset the form
   resetForm() {
-    this.todoForm.reset(); 
+    this.todoForm.reset();
   }
 
   handleTodoSubmission() {
-    const task = this.todoForm.value; 
-    console.log(task); 
-  
+    const task = this.todoForm.value;
+    console.log(task);
+
     let newTodo: todo[] = [
       {
-        task : task.todo  ,  
-        id: this.generateKey()
-      }
-    ]
+        task: task.todo,
+        id: this.generateKey(),
+      },
+    ];
 
     let updateTodo: todo = {
-      task : task.todo  ,  
-      id : this.generateKey()
-    }
+      task: task.todo,
+      id: this.generateKey(),
+    };
 
-   
     //create the key for getting the todos
-    const key = getUsernameAsKey(); 
+    const key = getUsernameAsKey();
 
-    //checking to see if the user already have some todos there 
-    const areSomeTodosThere = localStorage.getItem(key); 
+    //checking to see if the user already have some todos there
+    const areSomeTodosThere = localStorage.getItem(key);
 
     if (areSomeTodosThere) {
-      const todos = JSON.parse(areSomeTodosThere); 
-      todos.push(updateTodo); 
+      const todos = JSON.parse(areSomeTodosThere);
+      todos.push(updateTodo);
 
-      let updateTodos = JSON.stringify(todos); 
+      let updateTodos = JSON.stringify(todos);
 
-      // store them 
-      localStorage.setItem(key, updateTodos); 
+      // store them
+      localStorage.setItem(key, updateTodos);
 
-      // reset the form 
-      this.resetForm(); 
+      // reset the form
+      this.resetForm();
 
-      // get all the todos after an update 
-      getAllUserTodos(); 
+      // get all the todos after an update
+      getAllUserTodos();
 
-      alert('Todos have been updated'); 
+      this.notify.showSuccess("Updated TodoList", "");
     } else {
-        // stringifying the newTodo object to store 
-      const Todo = JSON.stringify(newTodo); 
+      // stringifying the newTodo object to store
+      const Todo = JSON.stringify(newTodo);
 
-    //storing the todo in the local storage 
+      //storing the todo in the local storage
       localStorage.setItem(key, Todo);
 
-      // get all the todos after an update 
-      getAllUserTodos(); 
-      
-      //reset the form 
-      this.resetForm();
-      
-      alert('New todo added'); 
+      // get all the todos after an update
+      getAllUserTodos();
 
+      //reset the form
+      this.resetForm();
+
+      this.notify.showSuccess("New Todod Added", "");
     }
   }
 
   handleLogout() {
     // we will ask if the wants to really logout
-    // let's remove the token 
-    localStorage.removeItem('token'); 
-    localStorage.removeItem('store'); 
-    this.router.navigateByUrl('/login'); 
+    // let's remove the token
+    localStorage.removeItem("token");
+    localStorage.removeItem("store");
+    this.router.navigateByUrl("/login");
   }
 
+  // handle todo delete
+  handleDelete(id: number) {
+    this.notify.showConfirmation('Are you sure you want to delete ?', 'You cannot revert this ').then(result => {
+      if (result.isConfirmed) {
+        let userTodos = getAllUserTodos();
+        // let's find the particular todo
+        let todo = userTodos?.find((todo) => todo.id == id);
+        console.log(todo);
+      }
+    });
+  }
 }
